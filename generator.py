64567946eef6384d827351e1d0214bf150a9249c
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2015-03-08 01:36:17 +0300

    navigation template moved from source code + a minor restyle

--- generator.py
+++ generator.py
@@ -8,28 +8,18 @@ from os.path import join
 
 INPUT = 'images.txt'
 GALLEY_PAGE = 'gallery_page_template.html'
+NAVIGATION = 'navigation_template.html'
 OUTPUT_PATH = '../'
 
-PREV_TEMPLATE = '''
-   <div style="position: absolute; left: 0px; top: 0px; width:64px; padding: 20px;">
-    <a href="{}" style="color: white;">
-     <img src="techImages/prev.png" borger="0" width="64" height="64">
-    </a>
-   </div>
-'''
 
-NEXT_TEMPLATE = '''
-   <div style="position: absolute; right: 0px; top: 0px; width:64px; padding: 20px;">
-    <a href="{}" style="color: white;">
-     <img src="techImages/next.png" borger="0" width="64" height="64">
-    </a>
-   </div>
-'''
-
-def create_page(prev, this, next, template, output=OUTPUT_PATH):
+def create_page(prev, this, next, template, navigation_template, output):
     '''Put all data into page template'''
-    prev = '' if prev is None else PREV_TEMPLATE.format(prev['page'])
-    next = '' if next is None else NEXT_TEMPLATE.format(next['page'])
+    prev = '' if prev is None else navigation_template.format(side='left',
+                                                              filename=prev['page'],
+                                                              image='prev')
+    next = '' if next is None else navigation_template.format(side='right',
+                                                              filename=next['page'],
+                                                              image='next')
 
     html = template.format(title=this['title'],
                            prev=prev, next=next,
@@ -43,11 +33,14 @@ if __name__ == '__main__':
     with open(GALLEY_PAGE, 'r') as template_file:
         gallery_page_template = template_file.read()
 
+    with open(NAVIGATION, 'r') as template_file:
+        navigation_template = template_file.read()
+
 
     images = [None]
     with open(INPUT, 'r') as input_file:
         for line in input_file:
-            filename, title, description = line.split('\t')
+            filename, title, description = line.rstrip().split('\t')
             images.append({'image': filename,
                            'page': filename.split('.')[0]+'.html',
                            'title': title,
@@ -55,4 +48,7 @@ if __name__ == '__main__':
     images.append(None)
 
     for prev, this, next in zip(images[:-2], images[1:-1], images[2:]):
-        create_page(prev, this, next, gallery_page_template)
+        create_page(prev, this, next, 
+                    gallery_page_template,
+                    navigation_template,
+                    OUTPUT_PATH)
